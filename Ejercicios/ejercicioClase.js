c4f9a737c9ece51fac72a5fef40380763cc8d434
Amneris Calle <amneris.calle@gmail.com>
2024-01-29 06:47:48 -0300

    agregué archivos al git ignore

--- Ejercicios/ejercicioClase.js
+++
@@ -1,7 +0,0 @@
-const objeto = {};
-for (let i = 1; i <= 10000; i++) {
-  let numero = Math.floor(Math.random() * 20 + 1);
-  console.log(numero);
-  objeto[numero] = objeto[numero] ? objeto[numero] + 1 : 1;
-}
-console.log(objeto);
